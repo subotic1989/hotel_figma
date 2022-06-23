df38bd18525373526f4c9b7dbd6d559e861b414b
subotic1989 <subotic.dejan89@gmail.com>
2022-06-23 21:54:13 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,10 @@ import { AdvantagesSecComponent } from './components/advantages-sec/advantages-s
 import { RoomsComponent } from './components/rooms/rooms.component';
 import { CirclesComponent } from './components/circles/circles.component';
 import { TestimonialsComponent } from './components/testimonials/testimonials.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { BottomInfoComponent } from './components/bottom-info/bottom-info.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -23,6 +27,10 @@ import { TestimonialsComponent } from './components/testimonials/testimonials.co
     RoomsComponent,
     CirclesComponent,
     TestimonialsComponent,
+    BlogComponent,
+    BannerComponent,
+    BottomInfoComponent,
+    FooterComponent,
   ],
   imports: [BrowserModule, NavbarModule],
   bootstrap: [AppComponent],
