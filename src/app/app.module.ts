958e555e570ffe400a877b9386277ef47dbf07a5
subotic1989 <subotic.dejan89@gmail.com>
2022-06-23 08:40:07 +0200

    cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { LuxuryComponent } from './components/luxury/luxury.component';
+import { NavbarModule } from './components/navbar/navbar.module';
+import { WellnessComponent } from './components/wellness/wellness.component';
+import { RestaurantComponent } from './components/restaurant/restaurant.component';
+import { AdvantagesComponent } from './components/advantages2/advantages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponent,
+    LuxuryComponent,
+    WellnessComponent,
+    RestaurantComponent,
+    AdvantagesComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, NavbarModule],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
